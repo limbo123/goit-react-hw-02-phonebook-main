9323fb40b1e6deda9715293734162ee7f8ab1b5b
Oleg Ovramets <olegovramets@gmail.com>
2022-02-09 21:20:11 +0200

    finish homework

--- src/App.js
+++ src/App.js
@@ -16,20 +16,6 @@ class App extends React.Component {
     filter: "",
   };
 
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem("contacts"));
-    if(contacts) {
-      this.setState({ contacts });
-    }
-    
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if(prevState.contacts !== this.state.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-    }
-  }
-
   createForm = (name, number) => {
     if (
       this.state.contacts.every((el) => {
